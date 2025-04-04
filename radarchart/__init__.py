4c94f5a92710f68d0f9b2e997b5273313e6cb380
Sabir Ribas <sabirribas@gmail.com>
2025-04-04 22:25:09 +1100

    Plot function

--- radarchart/__init__.py
+++ radarchart/__init__.py
@@ -1,3 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
-def plot(df=None):
-    print('Not implemented yet.')
+
+def plot(df, norm_max=True, streamlit=False):
+    df = df.copy()
+    # if type(norm_max) == list:
+    df /= norm_max
+    # if norm_max:
+    #     df /= df.max()
+
+    angles = np.linspace(0, 2 * np.pi, len(df.columns), endpoint=False)
+    angles = np.concatenate((angles, [angles[0]]))  # close the plot
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, polar=True)
+
+    for i in df.index:
+        stats = df.loc[i, :].values
+        stats = np.concatenate((stats, [stats[0]]))  # close the plot
+        ax.plot(angles, stats, 'o-', linewidth=2, label=i)
+        ax.fill(angles, stats, alpha=0.25)
+
+    ax.legend(loc='upper left', bbox_to_anchor=(1, 1))
+
+    columns = list(df.columns) + list(df.columns[:1])
+    # print((angles * 180 / np.pi).shape, len(columns))
+    ax.set_thetagrids(angles * 180 / np.pi, columns)
+    ax.grid(True)
+    if streamlit:
+        return fig, ax
+    return ax
